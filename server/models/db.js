b02ca011f3fe4d0791401ae989c9eeec75a8c728
Ayush Katiyar <ayushkatiyar972194@gmail.com>
2025-11-10 21:42:10 +0530

    Fix database connection configuration by adding missing port and connectTimeout

--- server/models/db.js
+++ server/models/db.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectTimeout: 20000
 });
 
 db.connect(err => {
